4baa72e7d87b2e0cd0257b92f604e994e95331b4
Ali Sinan Coban <alisinancoban@gmail.com>
2019-04-23 19:05:28 +0300

    dynamic styling with css classes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,14 @@
 new Vue({
     el: '#app',
     data: {
-        counter: 0,
-        sndCounter: 0
+        attachRed: false
     },
     computed: {
-        output: function(){
-            //calling only propery changed
-            console.log("computed");
-            return this.counter > 5 ? "Greater 5" : "Smaller 5";
-        }
-    },
-    watch: {
-        counter: function(value /*vuejs automatically passes*/){
-            var vm = this;
-            console.log("watch");
-            setTimeout(function(){
-                vm.counter = 0;
-            },2000);
-        }
-    },
-    methods: {
-        result: function(){
-            //Calling every time
-            console.log("method");
-            return this.counter > 5 ? "Greater 5" : "Smaller 5";        
+        divClasses : function(){
+            return {
+                red: this.attachRed,
+                blue: !this.attachRed
+            }
         }
     }
-})
+});
