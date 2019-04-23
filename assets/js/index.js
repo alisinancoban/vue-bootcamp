72d5765407c1c441f71050faaeb0f3797adc44c1
Ali Sinan Coban <alisinancoban@gmail.com>
2019-04-23 17:00:43 +0300

    watching for change

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,12 +11,20 @@ new Vue({
             return this.counter > 5 ? "Greater 5" : "Smaller 5";
         }
     },
+    watch: {
+        counter: function(value /*vuejs automatically passes*/){
+            var vm = this;
+            console.log("watch");
+            setTimeout(function(){
+                vm.counter = 0;
+            },2000);
+        }
+    },
     methods: {
-        result(){
+        result: function(){
             //Calling every time
             console.log("method");
-            return this.counter > 5 ? "Greater 5" : "Smaller 5";
-            
+            return this.counter > 5 ? "Greater 5" : "Smaller 5";        
         }
     }
 })
