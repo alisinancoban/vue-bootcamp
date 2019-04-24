0fb24a51cc89211cbc6805f09660b3c98c644ea2
Ali Sinan Coban <alisinancoban@gmail.com>
2019-04-24 20:46:49 +0300

    v-for

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,16 @@
 var app = new Vue({
     el: "#app",
     data: {
-        show: true
+        ingredients: ["meat", "fruit", "cookies"],
+        persons: [
+            {name: "Sinan", age: 23, color: "blue"},
+            {name: "Alper", age: 21, color: "red"}
+        ],
+        input: "yogurt"
+    },
+    methods: {
+        pushToList: function(){
+            this.ingredients.push(this.input);
+        }
     }
 });
